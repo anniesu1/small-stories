4614687f415fff02d052279a90575407cd647d7e
anniesu1 <anniesumail@gmail.com>
2019-10-25 03:27:40 -0400

    implemented basic working story with cat

--- planner/test.js
+++ planner/test.js
@@ -1,25 +1,42 @@
 var strips = require('strips');
 
 // Load the domain and problem.
-//export function GetBFSSolutions() {
-  strips.load('./dinner_domain.txt', './dinner_problem.txt', function(domain, problem) {
-      // Run the problem against the domain.
-      // var propValue;
-      // for(var propName in domain) {
-      //   propValue = domain[propName];
-      //   console.log(propName, propValue);
-      // }
-      var solutions = strips.solve(domain, problem);
+strips.load('./dinner_domain.txt', './dinner_problem.txt', function(domain, problem) {
+    // Run the problem against the domain.
+    // var propValue;
+    // for(var propName in domain) {
+    //   propValue = domain[propName];
+    //   console.log(propName, propValue);
+    // }
+    var solutions = strips.solve(domain, problem);
 
-      // Display each solution.
-      for (var i in solutions) {
-          var solution = solutions[i];
+    // Display each solution.
+    for (var i in solutions) {
+        var solution = solutions[i];
 
-          console.log('- Solution found in ' + solution.steps + ' steps!');
-          for (var i = 0; i < solution.path.length; i++) {
-              console.log((i + 1) + '. ' + solution.path[i]);
-          }
-      }
-  });
-//  return "hello goobs";
-//}
+        console.log('- Solution found in ' + solution.steps + ' steps!');
+        for (var i = 0; i < solution.path.length; i++) {
+            console.log((i + 1) + '. ' + solution.path[i]);
+        }
+    }
+});
+
+strips.load('./story_world_domain.txt', './story_world_problem.txt', function(domain, problem) {
+    // Run the problem against the domain.
+    // var propValue;
+    // for(var propName in domain) {
+    //   propValue = domain[propName];
+    //   console.log(propName, propValue);
+    // }
+    var solutions = strips.solve(domain, problem);
+
+    // Display each solution.
+    for (var i in solutions) {
+        var solution = solutions[i];
+
+        console.log('- Solution found in ' + solution.steps + ' steps!');
+        for (var i = 0; i < solution.path.length; i++) {
+            console.log((i + 1) + '. ' + solution.path[i]);
+        }
+    }
+});
