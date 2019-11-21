2999c37fc29e0ccc2c6b7576686ad394b5fc8c5e
anniesu1 <anniesumail@gmail.com>
2019-11-20 23:16:10 -0500

    implemented sentence templates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
+import axios from 'axios';
 //import { GetBFSSolutions } from '../planner/test.js';
 //import StoryText from '../components/story_text'
 
